0b86d71fbaab9efeb2fec8e44e2ca37c5a524ae5
ALBERT CORENTIN p2203615 <corentin.albert@etu.univ-lyon1.fr>
2023-05-26 12:21:22 +0000

    erreur fixée : ajout des autorisations pour requetes post

--- db/api.py
+++ db/api.py
@@ -8,7 +8,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"]
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 database = r"bdd.db"
